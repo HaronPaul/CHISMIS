afed13e3c4deda61005894aee314671e30d3d176
HaronPaul <hplorente@up.edu.ph>
2022-03-13 23:18:23 +0800

    added rendering of attendance on attendanceDoc

--- server/controller/attendanceController.js
+++ server/controller/attendanceController.js
@@ -1,13 +1,16 @@
+const _ = require('lodash')
+
 const ControlRoom = require('../model/ControlRoom')
 const HCl = require('../model/HCl')
 const Evaporator = require('../model/Evaporator')
 const PrimaryBrine = require('../model/PrimaryBrine')
 const Electrolysis = require('../model/Electrolysis')
 const NaClO = require('../model/NaClO')
+const data = require('./tempData')
 
-// Function helper for retrieving operators
-// Parameters: Models
-const retrieveOperators = async (Section1, month, year) => {   
+// Function helper for retrieving attendance of each operator
+// Parameters: Model, month in int, and year in int
+const retrieveOperatorsShift = async (Section1, month, year) => {   
     try {
         const section1Reponse =  await Section1.aggregate([  
             {$project: {
@@ -35,38 +38,116 @@ const retrieveOperators = async (Section1, month, year) => {
     }
 }
 
+// Function helper for retrieving names of operator from each section
+// Parameters: Model, month in int, and year in int
+const retrieveOperators = async (Section, month, year) => {
+    try {
+        const operatorsResponse = await Section.aggregate(
+            [
+                {$project: {
+                    present_operator: 1, 
+                    shift: 1, 
+                    date: 1, 
+                    month: {
+                      $month: '$date'
+                    }, 
+                    year: {
+                      $year: '$date'
+                    }
+                }}, 
+                {$match: {
+                    month: month,
+                    year: year
+                }},
+                {$group: {
+                    _id: null, 
+                    operators: {
+                      $addToSet: '$present_operator'
+                    }
+                }}
+              ]
+        )
+        return operatorsResponse
+    } catch(err) {
+        return err
+    }
+}
+
+const markAttendanceMap = (attendanceMap, section) => {
+    section.forEach((d) => {
+        const day = parseInt((d._id.dateString).split('-')[2])
+        // Loop throught each shift and set the value in the array in the attendance map
+        d.operators.forEach((op) => {
+            if(attendanceMap.get(op.present_operator)[day-1] === '') {
+                attendanceMap.get(op.present_operator)[day-1] = (op.shift).toString()
+            } else {
+                attendanceMap.get(op.present_operator)[day-1] = attendanceMap.get(op.present_operator)[day-1] + "/" + (op.shift).toString()
+            }
+        })
+    })
+}
 
 const getOperators = async (req,res) => {
     const month = parseInt(req.params.month)
     const selectedSection = parseInt(req.params.section)
     const year = parseInt(req.params.year)
 
+    let sections = []
     let section1
-    let section2 
-    try {
+    let section2
+    let operators1
+    let operators2
 
-        // If 1, retrieve from Control Room and HCl Sections
-        if(selectedSection === 1) {
-            section1 = await retrieveOperators(ControlRoom, month, year)
-            section2 = await retrieveOperators(HCl, month, year)
-        } 
-        // If 2, retrieve from Primary Brine and Evap
-        else if(selectedSection === 2) {
-            section1 = await retrieveOperators(PrimaryBrine, month, year)
-            section2 = await retrieveOperators(Evaporator, month, year)
-        // If 3, retrieve from NaClO and Electro 
-        } else if(selectedSection === 3) {
-            section1 = await retrieveOperators(NaClO, month, year)
-            section2 = await retrieveOperators(Electrolysis, month, year)
+    try {
+        // If 1, retrieve from Control Room and HCl Sections, 2 retrieve from Primary Brine and Electrolysis
+        switch(selectedSection) {
+            case 1: 
+                sections = [...[ControlRoom, HCl]]
+                break
+            case 2: 
+                sections = [...[PrimaryBrine, Electrolysis]]
+                break
+            case 3: 
+                sections = [...[NaClO, Evaporator]]
+                break
+            default: sections = [...[ControlRoom, HCl]]
         }
 
-        if(section1 && section2) {
-            console.log(section1)
+        section1 = await retrieveOperatorsShift(sections[0], month, year)
+        section2 = await retrieveOperatorsShift(sections[1], month, year)
+
+        if(section1.length > 0 && section2.length > 0) {
+            // Get the operators
+            operators1 = await retrieveOperators(sections[0], month, year)
+            operators2 = await retrieveOperators(sections[1], month, year)
+
+            // Map's keys are the name of the operators. Value of each key is an array containing (n days of months) elements  
+            var attendanceMap = new Map()
+            const sectionOperators = _.union(operators1[0].operators, operators2[0].operators)
+            // const sectionOperators = ['Haron Paul Lorente', 'Hannah Patriz Lorente', 'Jazon Troy Jaralve']
+            
+            // Get the number of days. This will be used for the size of the array
+            const numOfDays = new Date(year, month, 0).getDate()
+            sectionOperators.forEach((op)=> {
+                attendanceMap.set(op, new Array(numOfDays).fill(''))
+            })
+
+            // Set the attendance from the first section
+            // Loop through each item in the array. ID is the date
+            markAttendanceMap(attendanceMap, section1)
+            markAttendanceMap(attendanceMap, section2)
+
+            console.log(attendanceMap) 
+
             res.json({
                 success: true,
                 message: 'Successfully retrieved operators',
-                section1: section1,
-                section2: section2
+                data: Object.fromEntries(attendanceMap) 
+            })
+        } else {
+            return res.json({
+                success: false,
+                message: 'No attendance records from selected month'
             })
         }
 
@@ -74,7 +155,7 @@ const getOperators = async (req,res) => {
         console.log(err)
         res.status(400).json({
             success: false,
-            message: 'Something went wrong querying from database',
+            message: 'Something went wrong in querying from database',
             err,
         })
     }
--- server/routes/atttendance.js
+++ server/routes/atttendance.js
@@ -2,6 +2,6 @@ const router = require('express').Router()
 const {getOperators} = require('../controller/attendanceController')
 const checkConnection = require('../middleware/connectionCheck')
 
-router.get('/:year/:month/:section', checkConnection, getOperators)
+router.get('/:year/:month/:section', getOperators)
 
 module.exports = router
